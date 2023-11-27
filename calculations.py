12ba1aef3be55058b0d1e97aea922d08a34fae0d
Vinicius Santos <dukeofgod@outlook.com>
2023-11-27 10:04:22 -0300

    calculations

--- calculations.py
+++ calculations.py
@@ -5,31 +5,41 @@ import numpy as np
 import base64
 
 
+def valores_pago(valor_pago_mes:[],total_pago_ano:int, tempo:int,taxa):
+    for i in range(1, tempo+1):
+        num = valor_pago_mes[i - 1] * (1 + taxa/100)
+        valor_pago_mes.append(num)
+        total_pago_ano+= num*12
+
+    
 def index(valor_compra ,
 	valor_entrada,
 	tempo,
-	valor_aluguel,valorizacao_imovel,
+	valor_aluguel,
+    valorizacao_imovel,
 	taxa_financiamento):
 
     # x = symbols('x')
-    a = 3000  # You can adjust the value of 'a' as needed
-
     # Equation: a * (1 + 0.1)^x
     #y = a * (1 + 0.1) ** x
-    valor_pago_mes = [2500]
-    total_pago_ano = [(2500*12)]
-    tempo = 10
+    parcela = valor_compra-valor_entrada/tempo
+    valor_pago_mes_compra = [parcela]
+    total_pago_ano_compra = 0
+    valor_pago_mes_aluguel = [valor_aluguel]
+    total_pago_ano_aluguel = 0
 
-    for i in range(1, tempo+1):
-        num = valor_pago_mes[i - 1] * (1 + 0.1)
-        valor_pago_mes.append(num)
-        total_pago_ano.append(num*12)
-    print(valor_pago_mes)
-    print(total_pago_ano)
+    valores_pago(valor_pago_mes_compra,total_pago_ano_compra,tempo,taxa_financiamento)
+    valores_pago(valor_pago_mes_aluguel,total_pago_ano_aluguel,tempo,valorizacao_imovel)
 
-    (valor_compra-valor_entrada * (1 + 0.1) ^ tempo/12 )+valor_entrada #  -> Em quanto tempo minimo é necessário para que o somatório seja maior que o totalcomprafinanceada
 
-    # Generate x values for the plot
+
+    (valor_compra-valor_entrada * (1 + 0.1) ^ tempo )+valor_entrada #  -> Em quanto tempo minimo é necessário para que o somatório seja maior que o totalcomprafinanceada
+    number = 0
+    minNum = 1
+    while number > 0:
+        number=(valor_compra-valor_entrada * (1 + 0.1) ^ tempo )+valor_entrada
+        
+        
     # x_values = np.linspace(1, 10, 100)
     # y_values = [y.evalf(subs={x: val}) for val in x_values]
     #
