f7f859d4c4b90b14018df64376b8b7aed14a0f97
Letícia Malato <leticiamalato3@gmail.com>
2023-11-26 22:34:49 -0300

    feat:pages

--- frontend/src/app/components/sections/Calc.tsx
+++ frontend/src/app/components/sections/Calc.tsx
@@ -13,36 +13,45 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
-export function Contact() {
+export function Calc() {
     return (
       <section
-        className="mt-24 flex w-full flex-col items-center justify-center gap-9 px-20 max-[1234px]:flex-wrap max-md:px-4 md:justify-center"
+        className="mt-24 flex w-full relative  min-h-[calc(100vh_-_230px)] flex-col items-center justify-center gap-9 px-20 max-[1234px]:flex-wrap max-md:px-4 md:justify-center"
         id="contato"
       >
-        <Heading size="xl" className="w-fit max-md:text-center" color="text-black-900">
-          Entre em contato conosco!
+       <Heading
+            size="2xl"
+            className=" w-fit bg-blue-950"
+            color="text-white"
+          >
+            Faça o cálculo!
         </Heading>
   
-        <div className="mx-auto grid max-w-4xl gap-10 md:grid-cols-2">
+        <div className="flex w-180 flex-col items-center justify-center gap-4 rounded-lg bg-white p-4 shadow-md group">
           <div>
             <div className="mt-5">
 
           </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+          <div className="grid w-full max-w-sm items-center gap-1.5 mt-5  text-black">
       <Label htmlFor="aluguel">Digite o valor do aluguel do imovel</Label>
       <Input id="aluguel" type="number" />
     </div>
 
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="compra">Digite o valor da compra do imovel</Label>
+    <div className="grid w-full max-w-sm items-center gap-1.5 mt-5  text-black">
+      <Label htmlFor="compra">Digite o valor do imovel</Label>
+      <Input id="compra" type="number" />
+    </div>
+    <div className="grid w-full max-w-sm items-center gap-1.5 mt-5  text-black">
+      <Label htmlFor="compra">Digite o valor da entrada</Label>
       <Input id="compra" type="number" />
     </div>
 
-    <div className="grid w-full max-w-sm items-center gap-1.5">
+    <div className="grid w-full max-w-sm items-center gap-1.5 mt-5  text-black">
       <Label htmlFor="tempo">Digite o tempo, em meses, que você pretende ficar no imovel</Label>
       <Input id="tempo" type="number" />
     </div>
-    <div className="mt-2 flex items-center space-x-2 text-gray-600">
+    <div className="mt-5 flex flex-col justify-start items-center space-x-2 text-black " >
+    
               <span>
                 Agora você deve escolher entre dois cenários:
               </span>
@@ -53,22 +62,21 @@ export function Contact() {
                 Cenário 2: 
               </span>
             </div>
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Escolha a forma de financiamento" />
+            <Select>
+      <SelectTrigger className="w-[280px] bg-blue-950">
+        <SelectValue placeholder="Escolha seu financiamento" />
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup>
+        <SelectGroup className="bg-black">
           <SelectLabel>Financiamentos</SelectLabel>
-          <SelectItem value="apple">cenário 1</SelectItem>
-          <SelectItem value="banana">cenaário 2</SelectItem>
+          <SelectItem value="Cenario1">Cenário 1</SelectItem>
+          <SelectItem value="Cenatio2">Cenário 2</SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
-  
-  <Button variant="secondary">Calcular</Button>
-
-
+    <div className="flex w-20 h-10 mt-5 mb-10 justify-center bg-blue-950 ">
+    <Button variant="secondary">Calcular</Button>
+    </div>
           </div>
         </div>
       </section>
